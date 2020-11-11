7195d39117777160a3a76cb1040963fd97587c51
Daniel Archer <daniel.archer.dev@hotmail.com>
2020-11-11 13:03:34 -0400

    Sample drag and drop to and from listbox, with click and drag re-ordering

---
+++ Sample_WindowsForms/Drag and Drop Process Queue/Process Queue.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Drag_and_Drop_Process_Queue
+{
+    partial class ProcessQueue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox_left = new System.Windows.Forms.ListBox();
+            this.listBox_right = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Location = new System.Drawing.Point(0, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 423);
+            this.panel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox_left);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBox_right);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 423);
+            this.splitContainer1.SplitterDistance = 419;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listBox_left
+            // 
+            this.listBox_left.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_left.FormattingEnabled = true;
+            this.listBox_left.Location = new System.Drawing.Point(0, 0);
+            this.listBox_left.Name = "listBox_left";
+            this.listBox_left.Size = new System.Drawing.Size(419, 423);
+            this.listBox_left.TabIndex = 0;
+            this.listBox_left.DragDrop += new System.Windows.Forms.DragEventHandler(this.generic_DragDrop);
+            this.listBox_left.DragEnter += new System.Windows.Forms.DragEventHandler(this.generic_DragEnter);
+            this.listBox_left.DragOver += new System.Windows.Forms.DragEventHandler(this.generic_DragOver);
+            this.listBox_left.DragLeave += new System.EventHandler(this.generic_DragLeave);
+            this.listBox_left.MouseDown += new System.Windows.Forms.MouseEventHandler(this.generic_MouseDown);
+            // 
+            // listBox_right
+            // 
+            this.listBox_right.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_right.FormattingEnabled = true;
+            this.listBox_right.Location = new System.Drawing.Point(0, 0);
+            this.listBox_right.Name = "listBox_right";
+            this.listBox_right.Size = new System.Drawing.Size(377, 423);
+            this.listBox_right.TabIndex = 0;
+            this.listBox_right.DragDrop += new System.Windows.Forms.DragEventHandler(this.generic_DragDrop);
+            this.listBox_right.DragEnter += new System.Windows.Forms.DragEventHandler(this.generic_DragEnter);
+            this.listBox_right.DragOver += new System.Windows.Forms.DragEventHandler(this.generic_DragOver);
+            this.listBox_right.DragLeave += new System.EventHandler(this.generic_DragLeave);
+            this.listBox_right.MouseDown += new System.Windows.Forms.MouseEventHandler(this.generic_MouseDown);
+            // 
+            // ProcessQueue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "ProcessQueue";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox_left;
+        private System.Windows.Forms.ListBox listBox_right;
+    }
+}
+
---
+++ Sample_WindowsForms/Drag and Drop Process Queue/Process Queue.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drag_and_Drop_Process_Queue
+{
+    public partial class ProcessQueue : Form
+    {
+        public ProcessQueue()
+        {
+            InitializeComponent();
+
+            listBox_left.AllowDrop = true;
+            listBox_right.AllowDrop = true;
+
+        }
+
+
+
+        private void generic_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Move;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private string[] FilePicker_DroppedFile(DragEventArgs e)
+        {
+            return (string[])e.Data.GetData(DataFormats.FileDrop);
+        }
+
+        private void AddArray_To_ListBox(string[] list, ListBox listBox)
+        {
+            foreach (string item in list)
+            {
+                listBox.Items.Add(item);
+            }
+        }
+        private void AddString_To_ListBox(string item, ListBox listBox)
+        {
+            listBox.Items.Add(item);
+        }
+
+        private void generic_DragDrop(object sender, DragEventArgs e)
+        {
+            // add a file that was dropped over
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                AddArray_To_ListBox(FilePicker_DroppedFile(e), (ListBox)sender);
+            else if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                AddString_To_ListBox((String)e.Data.GetData(DataFormats.Text), (ListBox)sender);
+            }
+            // but move a list item that was clicked on
+            else
+            {
+                generic_MoveItem(sender, e);
+            }
+        }
+        private void generic_DragLeave(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                // TODO: get underlying data and add to list
+                //AddString_To_ListBox(e.Data.ToString(), (ListBox)sender);
+
+                // TODO: don't forget to remove from source list
+                ListBox listBox = (ListBox)sender;
+                Point point = listBox.PointToClient(new Point(e.X, e.Y));
+                int index = listBox.IndexFromPoint(point);
+                if (index < 0) index = listBox.Items.Count - 1;
+                object data = e.Data.GetData(typeof(String));
+                listBox.Items.Remove(data);
+                //listBox.Items.Insert(index, data);
+            }
+        }
+        private void generic_MoveItem(object sender, DragEventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            Point point = listBox.PointToClient(new Point(e.X, e.Y));
+            int index = listBox.IndexFromPoint(point);
+            if (index < 0) index = listBox.Items.Count - 1;
+            object data = e.Data.GetData(typeof(String));
+            listBox.Items.Remove(data);
+            listBox.Items.Insert(index, data);
+        }
+
+        private void generic_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.Move;
+        }
+        private void generic_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            if (listBox.SelectedItem == null) return;
+            listBox.DoDragDrop(listBox.SelectedItem, DragDropEffects.Move);
+        }
+
+        private void generic_DragLeave(object sender, EventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            listBox.Items.Remove(listBox.SelectedItem);
+        }
+    }
+}
