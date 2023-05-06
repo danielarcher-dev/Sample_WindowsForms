ed6b1384bff96e4561987cccc8fd271b19907a5d
danielarcher-dev <daniel.archer.dev@outlook.com>
2023-05-05 21:10:32 -0300

    implement simple chess timer. very bare bones, needs refinement. timer isn't clock accurate, and counts up instead of down.

--- Sample_WindowsForms/Simple Chess Timer/Form1.Designer.cs
+++ Sample_WindowsForms/Simple Chess Timer/Form1.Designer.cs
@@ -30,12 +30,82 @@ namespace Simple_Chess_Timer
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(369, 337);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(419, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(369, 337);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(419, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(369, 62);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(368, 62);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Sample_WindowsForms/Simple Chess Timer/Form1.cs
+++ Sample_WindowsForms/Simple Chess Timer/Form1.cs
@@ -12,9 +12,58 @@ namespace Simple_Chess_Timer
 {
     public partial class Form1 : Form
     {
+        decimal counter1 = 0;
+        decimal counter2 = 0;
+
         public Form1()
         {
             InitializeComponent();
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+
+            timer1.Interval = 10;
+            timer2.Interval = 10;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Green;
+            button2.BackColor = Color.Red;
+            timer1.Start();
+            timer2.Stop();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Green;
+            button1.BackColor = Color.Red;
+            timer2.Start();
+            timer1.Stop();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            update_textbox1();
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            update_textbox2();
+
+        }
+
+        private void update_textbox1()
+        {
+            textBox1.Text = counter1.ToString();
+            counter1 = counter1 + (decimal)0.01;
+        }
+
+        private void update_textbox2()
+        {
+            counter2 = counter2 + (decimal)0.01;
+            textBox2.Text = counter2.ToString();
         }
     }
 }
