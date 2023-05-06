477f41ed5885232c81db9dd32dfc1a5face2db50
Daniel Archer <Daniel.Archer.dev@outlook.com>
2023-05-06 08:44:39 -0300

    we have a functional app which adds screenshots to a list and a button to increment through the images

---
+++ Sample_WindowsForms/UWP_ImageGrid/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_ImageGrid
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        List<BitmapImage> imageList = new List<BitmapImage>();
+        string message = "";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //Gallery.ItemsSource = imageList;
+            ShowNext.Content = "Show next image";
+        }
+
+        async private void add_screenshot_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the bitmap and display it.
+            var dataPackageView = Clipboard.GetContent();
+            if (dataPackageView.Contains(StandardDataFormats.Bitmap))
+            {
+                IRandomAccessStreamReference imageReceived = null;
+                try
+                {
+                    imageReceived = await dataPackageView.GetBitmapAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //MainPage.NotifyUser("Error retrieving image from Clipboard: " + ex.Message, /*NotifyType*/.ErrorMessage);
+                }
+
+                if (imageReceived != null)
+                {
+                    using (var imageStream = await imageReceived.OpenReadAsync())
+                    {
+                        var bitmapImage = new BitmapImage();
+                        bitmapImage.SetSource(imageStream);
+                        //Screenshot.Source = bitmapImage;
+                        //Screenshot.Visibility = Visibility.Visible;
+                        imageList.Add(bitmapImage);
+                        message = "Image is retrieved from the clipboard and pasted successfully.";
+                    }
+                }
+            }
+            else
+            {
+                message = "Bitmap format is not available in clipboard";
+                //Screenshot.Visibility = Visibility.Collapsed;
+            }
+
+            message_block.Text = message;
+
+        }
+
+        int position = -1;
+        private void ShowNext_Click(object sender, RoutedEventArgs e)
+        {
+            position++;
+            ShowNext.Content = String.Format("showing position {0}", position);
+            try
+            {
+                if (imageList.Count == 0)
+                {
+                    message = "there are no images to show";
+                    position = -1;
+                }
+                else if(position < imageList.Count)
+                {
+                    ViewBoxImage.Source = imageList[position];
+                    message = String.Format("this is a string{0}", position);
+                }
+                else if (position >= imageList.Count)
+                {
+                    position = 0;
+                    message = String.Format("end of the list, resetting {0}", position);
+                    // we can safely do this, because we know it will never be called unless position >= imageList
+                    // if position=0 and imageList.Count=0?
+
+                    ViewBoxImage.Source = imageList[position];
+                }
+                message_block.Text = message;
+            }
+            catch
+            {
+                message_block.Text = "there was an error";
+            }
+
+            
+
+        }
+
+        private void Control1_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
